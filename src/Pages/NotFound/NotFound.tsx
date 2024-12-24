c550c00a0f6376f64bbfcc960e5f33dc2899ceea
Mohammed19200 <midoelkassaby2@gmail.com>
2024-12-24 07:40:31 +0200

    commit9

--- src/Pages/NotFound/NotFound.tsx
+++ src/Pages/NotFound/NotFound.tsx
@@ -1,9 +1,28 @@
-
+import { useNavigate } from "react-router-dom";
+import "./NotFound.css";
 
 export default function NotFound() {
+  let navigate = useNavigate();
+
   return (
-    <div>
-      NotFound
+    <div className="col-12 bigestdivnotfound wow animate__animated animate__fadeInDownBig animate__slow">
+      <div className="bigdivnotfound"></div>
+
+      <div className="col-12 divbuttonnotfound">
+        <h1 className="h6 h1titlenotfound col-11 col-md-8 col-lg-7 col-xl-5">
+          The page you are looking for does not exist. How you got here is a
+          mystery. But you can click the button below to go back to the
+          homepage.
+        </h1>
+        <button
+          onClick={() => {
+            navigate("/dashboard/home");
+          }}
+          className="buttonnotfound"
+        >
+          Go To Home
+        </button>
+      </div>
     </div>
-  )
+  );
 }
